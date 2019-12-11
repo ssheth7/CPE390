4ba9b2c72120a45202168e1e7f290ea524d87209
ssheth7 <54548775+ssheth7@users.noreply.github.com>
2019-12-10 20:16:48 -0500

    Update lab.cc

--- lab12/lab.cc
+++ lab12/lab.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-extern int mult(int x);
+extern int mult(int x, int y);//multiplies x and y using logical shifts
 int main(){
-  cout << mult(8) << '\n';
+  cout << mult(8, 7) << '\n';
 }
